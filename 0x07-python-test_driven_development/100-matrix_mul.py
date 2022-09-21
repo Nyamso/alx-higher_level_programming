cc6fcffcf738a5b05efffeeaddfaa17e222d87c8
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-21 17:38:30 +0200

    works

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Module matrix_mul
+Multiplies two matrices and returns the result.
+"""
+
+
 def matrix_mul(m_a, m_b):
+    """Return the matrix resulting of
+    the multiplication of m_a and m_b."""
+
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
@@ -30,12 +38,12 @@ def matrix_mul(m_a, m_b):
     for row in m_a:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_a must should be of the same size")
+            raise TypeError("each row of m_a must should be of the same size")
     row_len = []
     for row in m_b:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_b must should be of the same size")
+            raise TypeError("each row of m_b must should be of the same size")
 
     a_col = 0
     for col in m_a[0]:
