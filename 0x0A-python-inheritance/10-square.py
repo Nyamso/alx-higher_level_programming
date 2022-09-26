b0b5993506fd3e0737236517eca569f22d0d1ae7
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-26 23:45:10 +0200

    Initial Commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
+"""Module 10-square.
+Creates a Square class.
+"""
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-
 class Square(Rectangle):
-     """inherits from Rectangle, who inherits from BaseGeometry
-     methods:
-             __init__(self, size)
-     """
+    """Represents a square.
+    Private instance attribute size.
+    Public method area().
+    Inherits from Rectangle.
+    """
+
     def __init__(self, size):
-        """initializes size
+        """Initializes a Square.
+
         Args:
-             size (int): private
+            - size: size of the square
         """
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        return super().__str__()
+
+    def area(self):
+        """Computes the area of a Square instance.
+        Overwrites the area() method from Rectangle.
+        """
+
+        return self.__size ** 2
