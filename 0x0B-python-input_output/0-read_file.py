0420891b2b4026dcf58984893c6df2fa2ab40377
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-28 22:35:52 +0200

    updated task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""
-Module 0-read_file
-Contains function that reads and prints contents from file
+"""Module 0-read_file.
+Reads a certain number of lines from a file.
 """
 
 
-def read_file(filename=""):
-    """Read and print text from file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read(), end="")
+def read_lines(filename="", nb_lines=0):
+    """Reads and prints nb_lines lines from filename.
+
+    Args:
+        - filename: name of the file
+        - nb_lines: number of lines to read
+    """
+
+    with open(filename) as f:
+        i = 0
+        count = 0
+        for line in f:
+            count += 1
+        f.seek(0)
+        if nb_lines <= 0 or nb_lines >= count:
+            read_text = f.read()
+            print(read_text, end="")
+        else:
+            for line in f:
+                print(line, end='')
+                i += 1
+                if i == nb_lines:
+                    break
