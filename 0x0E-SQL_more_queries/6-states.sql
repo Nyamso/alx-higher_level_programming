cd22be3b57b628b3c5720ad34d7b81f72bd7e9c7
Nyamso <msosarehema@outlook.com>
2022-11-03 01:31:14 -0700

    updated queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,6 @@
 --creates the database hbtn_0d_usa and the table states on your MySQL server
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
-       PRIMARY KEY('id'),
-	'id' INT NOT NULL AUTO_INCREMENT,
-      'name' VARCHAR(256) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+       name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,8 @@
 -- Lists all cities in the database hbtn_0d_usa.
 
-SELECT c.'id', c.'name’' s. 'name'
-  FROM 'cities' AS c
-       INNER JOIN 'states' AS s
-       ON c.'state_id' = s.'id'
-	 ORDER BY c.'id';
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
 
 
