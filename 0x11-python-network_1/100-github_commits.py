cfb4b57f4bd20375c0e278c64b0953d00d72e7e6
Nyamso <msosarehema@outlook.com>
2022-12-20 13:26:07 -0800

    updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
     except IndexError:
         pass
