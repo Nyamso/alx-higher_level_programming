aef7045f9b538f3da5bc0e9abe08d25b338bd329
Nyamso <msosarehema@outlook.com>
2023-01-18 21:40:36 +0000

    update task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-let id = process.argv[2];
-let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body['title']);
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (err, response, body) {
+  if (err) { console.log(err); } else {
+    console.log(JSON.parse(body).title);
   }
 });
