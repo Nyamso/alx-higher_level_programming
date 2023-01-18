dbc02af3d826fac8183b62cdd0716e032b934e5b
Nyamso <msosarehema@outlook.com>
2023-01-18 21:42:43 +0000

    update task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-let filename = process.argv[3];
-const fs = require('fs');
 const request = require('request');
-
-request(url, function (err, response, body) {
+const fs = require('fs');
+const url = process.argv[2];
+request.get(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf8');
+    fs.writeFileSync(process.argv[3], body, 'utf-8', function (err, data) {
+      if (err) { console.log(err); } else { console.log(data); }
+    });
   }
 });
