41eddfee921441e9bdad96641eff2be697dc1f63
Nyamso <msosarehema@outlook.com>
2023-01-18 21:34:24 +0000

    Updating tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (err, response, body) {
+  if (err) { console.log(err); } else {
+    const cha = JSON.parse(body).characters;
+    const limit = cha.length;
+    Characters(0, cha[0], cha, limit);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
+function Characters (i, url, charList, limit) {
+  if (i === limit) { return; }
+  request.get(url, function (err, respinse, body) {
+    if (err) { console.log(err); } else {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+      i++;
+      Characters(i, charList[i], charList, limit);
     }
   });
 }
