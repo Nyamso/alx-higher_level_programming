7c295366537d6f77289d79ee7d77d83d602d6f2d
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2023-07-05 15:30:58 +0200

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script to get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
--- 0x10-python-network_0/4-header.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Scriot to send a GET request to a given URL with a header variable.
-curl -sH "X-School-User-Id: 98" "$1"
